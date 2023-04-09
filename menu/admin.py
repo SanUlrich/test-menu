ec4491e84d6ae9932779d5febb172fa797c13ec7
SanUlrich <SanUlrich@mail.ru>
2023-04-10 01:15:33 +0200

    add functionality to admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('item_title', 'item_slug', 'parent')
+    prepopulated_fields = {"item_slug": ("item_title",)}
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
